43234ec2bf90eb6142cc25ed8c00d871e829ea14
AngelLc18 <ciancioangel.epet20@gmail.com>
2023-08-28 14:32:41 -0300

    Creando projecto en react utilizando vite

--- js/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-  const toTop = document.querySelector(".to-top");
-  if (window.pageYOffset > 100) {
-    toTop.classList.add("active");
-  } else {
-    toTop.classList.remove("active");
-  }
-});
