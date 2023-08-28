2504ff41829f236b0e1ed236ddbbc4dccd284d53
AngelLc18 <ciancioangel.epet20@gmail.com>
2023-08-28 14:39:05 -0300

    update2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        style={{
+          width: 390,
+          height: 844,
+          position: "relative",
+          background: "white",
+        }}
+      >
+        <div
+          style={{
+            width: 281,
+            height: 105,
+            left: 55,
+            top: 663,
+            position: "absolute",
+            justifyContent: "center",
+            alignItems: "center",
+            display: "inline-flex",
+          }}
+        >
+          <img
+            style={{
+              width: 281,
+              height: 105,
+              boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+              borderRadius: 16,
+            }}
+            src="https://via.placeholder.com/281x105"
+          />
+        </div>
+        <div
+          style={{
+            width: 343,
+            height: 192,
+            left: 24,
+            top: 287,
+            position: "absolute",
+            justifyContent: "center",
+            alignItems: "center",
+            display: "inline-flex",
+          }}
+        >
+          <img
+            style={{ width: 343, height: 192 }}
+            src="https://via.placeholder.com/343x192"
+          />
+        </div>
+        <div
+          style={{
+            width: 252,
+            height: 96,
+            left: 69,
+            top: 217,
+            position: "absolute",
+            justifyContent: "center",
+            alignItems: "center",
+            display: "inline-flex",
+          }}
+        >
+          <img
+            style={{ width: 252, height: 96 }}
+            src="https://via.placeholder.com/252x96"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
