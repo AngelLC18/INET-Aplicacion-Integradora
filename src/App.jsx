78d483d821d4ddffd01f4b97a794e982b0979fa2
AngelLc18 <ciancioangel.epet20@gmail.com>
2023-08-28 16:38:32 -0300

    Cambios 28/08/2023

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,75 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="parent">
-      <div
-        className="bottom"
-        style={{
-          width: 281,
-          height: 105,
-          left: 55,
-          top: 663,
-          position: "absolute",
-          justifyContent: "center",
-          alignItems: "center",
-          display: "inline-flex",
-        }}
-      >
-        <img
-          style={{
-            width: 281,
-            height: 105,
-            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-            borderRadius: 16,
-          }}
-          src="/src/assets/Ministerio de Educación celeste.png"
-        />
-      </div>
-      <div
-        className="middle"
-        style={{
-          width: 343,
-          height: 192,
-          left: 24,
-          top: 287,
-          position: "absolute",
-          justifyContent: "center",
-          alignItems: "center",
-          display: "inline-flex",
-        }}
-      >
-        <img
-          style={{ width: 343, height: 192 }}
-          src="https://via.placeholder.com/343x192"
-        />
-      </div>
-      <div className="logo">
-        <img src="/src/assets/INET white.png" alt="inet-white" />
-      </div>
+    <div className="general-container">
+      <input className="radio" type="radio" name="card" id="card-1" checked />
+      <label className="content" for="card-1">
+        <span className="icon">
+          <i className="fas fa-sun"></i>
+        </span>
+        <h3 className="card-title">
+          Serra da Freita, Vale de Cambra, Portugal
+          <span className="subtitle">@hed</span>
+        </h3>
+      </label>
+      <input className="radio" type="radio" name="card" id="card-2" />
+      <label className="content" for="card-2">
+        <span className="icon">
+          <i className="fas fa-cloud-rain"></i>
+        </span>
+        <h3 className="card-title">
+          París, Paris, France
+          <span className="subtitle">@lolaguti</span>
+        </h3>
+      </label>
+      <input className="radio" type="radio" name="card" id="card-3" />
+      <label className="content" for="card-3">
+        <span className="icon">
+          <i className="fas fa-cloud-moon"></i>
+        </span>
+        <h3 className="card-title">
+          La Joue du Loup, Le Dévoluy, France
+          <span className="subtitle">@quentindrphotography</span>
+        </h3>
+      </label>
+      <input className="radio" type="radio" name="card" id="card-4" />
+      <label className="content" for="card-4">
+        <span className="icon">
+          <i className="fas fa-snowflake"></i>
+        </span>
+        <h3 className="card-title">
+          Jasper National Park, Jasper, Canada
+          <span className="subtitle">@punttim</span>
+        </h3>
+      </label>
+      <input className="radio" type="radio" name="card" id="card-5" />
+      <label className="content" for="card-5">
+        <span className="icon">
+          <i className="fas fa-poo-storm"></i>
+        </span>
+        <h3 className="card-title">
+          Sioux Falls, United States
+          <span className="subtitle">@eugenetriguba</span>
+        </h3>
+      </label>
+      <label className="content" for="card-6">
+        <span className="icon">
+          <i className="fas fa-poo-storm"></i>
+        </span>
+        <h3 className="card-title">
+          Sioux Falls, United States
+          <span className="subtitle">@eugenetriguba</span>
+        </h3>
+      </label>
+      <label className="content" for="card-7">
+        <span className="icon">
+          <i className="fas fa-poo-storm"></i>
+        </span>
+        <h3 className="card-title">
+          Sioux Falls, United States
+          <span className="subtitle">@eugenetriguba</span>
+        </h3>
+      </label>
     </div>
   );
 }
