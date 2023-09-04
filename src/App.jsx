9a0f8ebd44cee4e07cbb1547d4bbd4833af34934
Javier Martin Oberholzer <javier.oberholzer.epet20@gmail.com>
2023-09-04 13:53:58 -0300

    CHANGES

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,10 @@
 import "./App.css";
+ 
 
 function App() {
-  return (
-    <div className="parent">
-      <div
-        className="bottom"
-        style={{
-          width: 281,
-          height: 105,
-          top: 463,
-          position: "absolute",
-          justifyContent: "center",
-          alignItems: "center",
-          display: "inline-flex",
-        }}
-      >
-        <img
-          style={{
-            width: 281,
-            height: 105,
-            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-            borderRadius: 16,
-          }}
-          src="/src/assets/Ministerio de Educación celeste.png"
-        />
-      </div>
-      <div
-        className="middle"
-        style={{
-          width: 343,
-          height: 192,
-          
-          top: 187,
-          position: "absolute",
-          justifyContent: "center",
-          alignItems: "center",
-          display: "inline-flex",
-        }}
-    ><div class="wrapper">
-      <div class="block mover"></div>
-      <div class="block jumper"></div>
-      <div class="block jumper"></div>
-      <div class="block jumper"></div>
-      <div class="block jumper"></div>
-      
-    </div>
   
-        
-      </div>
-      <div className="logo" style={{ top:40 }}>
-        <img src="/src/assets/INET white.png" alt="inet-white" />
-      </div>
-    </div>
-  );
+
+  
 }
 
 export default App;
