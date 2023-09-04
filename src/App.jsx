83dd9624151c55cf8d03bb5c7d9938b97148cd64
AngelLc18 <ciancioangel.epet20@gmail.com>
2023-09-04 15:44:28 -0300

    A

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,11 @@
 import "./App.css";
-
-function App() {
+import Card from "./components/cards/card";
+const App = () => {
   return (
-    <div className="general-container">
-      <input className="radio" type="radio" name="card" id="card-1" checked />
-      <label className="content" for="card-1">
-        <span className="icon">
-          <i className="fas fa-sun"></i>
-        </span>
-        <h3 className="card-title">
-          Serra da Freita, Vale de Cambra, Portugal
-          <span className="subtitle">@hed</span>
-        </h3>
-      </label>
-      <input className="radio" type="radio" name="card" id="card-2" />
-      <label className="content" for="card-2">
-        <span className="icon">
-          <i className="fas fa-cloud-rain"></i>
-        </span>
-        <h3 className="card-title">
-          París, Paris, France
-          <span className="subtitle">@lolaguti</span>
-        </h3>
-      </label>
-      <input className="radio" type="radio" name="card" id="card-3" />
-      <label className="content" for="card-3">
-        <span className="icon">
-          <i className="fas fa-cloud-moon"></i>
-        </span>
-        <h3 className="card-title">
-          La Joue du Loup, Le Dévoluy, France
-          <span className="subtitle">@quentindrphotography</span>
-        </h3>
-      </label>
-      <input className="radio" type="radio" name="card" id="card-4" />
-      <label className="content" for="card-4">
-        <span className="icon">
-          <i className="fas fa-snowflake"></i>
-        </span>
-        <h3 className="card-title">
-          Jasper National Park, Jasper, Canada
-          <span className="subtitle">@punttim</span>
-        </h3>
-      </label>
-      <input className="radio" type="radio" name="card" id="card-5" />
-      <label className="content" for="card-5">
-        <span className="icon">
-          <i className="fas fa-poo-storm"></i>
-        </span>
-        <h3 className="card-title">
-          Sioux Falls, United States
-          <span className="subtitle">@eugenetriguba</span>
-        </h3>
-      </label>
-      <label className="content" for="card-6">
-        <span className="icon">
-          <i className="fas fa-poo-storm"></i>
-        </span>
-        <h3 className="card-title">
-          Sioux Falls, United States
-          <span className="subtitle">@eugenetriguba</span>
-        </h3>
-      </label>
-      <label className="content" for="card-7">
-        <span className="icon">
-          <i className="fas fa-poo-storm"></i>
-        </span>
-        <h3 className="card-title">
-          Sioux Falls, United States
-          <span className="subtitle">@eugenetriguba</span>
-        </h3>
-      </label>
+    <div>
+      <Card />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/cards/card.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import "./cards.css";
+import { useState } from "react";
+const Card = () => {
+  const [checkedCard, setCheckedCard] = useState("card-0");
+
+  const handleChange = (event) => {
+    setCheckedCard(event.target.id);
+  };
+
+  return (
+    <div className="general-container">
+      <input
+        className="radio"
+        type="radio"
+        name="card"
+        id="card-1"
+        checked={checkedCard === "card-1"}
+        onChange={handleChange}
+      />
+      <label className="content" for="card-1">
+        <span className="icon">
+          <i className="fas fa-sun"></i>
+        </span>
+        <h3 className="card-title">
+          Aulas Talleres Móviles
+          <span className="subtitle">@hed</span>
+        </h3>
+      </label>
+      <input
+        className="radio"
+        type="radio"
+        name="card"
+        id="card-2"
+        checked={checkedCard === "card-2"}
+        onChange={handleChange}
+      />
+      <label className="content" for="card-2">
+        <span className="icon">
+          <i className="fas fa-cloud-rain"></i>
+        </span>
+        <h3 className="card-title">
+          Inversión en Educación Técnico Profesional
+          <span className="subtitle">@lolaguti</span>
+        </h3>
+      </label>
+      <input
+        className="radio"
+        type="radio"
+        name="card"
+        id="card-3"
+        checked={checkedCard === "card-3"}
+        onChange={handleChange}
+      />
+      <label className="content" for="card-3">
+        <span className="icon">
+          <i className="fas fa-cloud-moon"></i>
+        </span>
+        <h3 className="card-title">
+          Catálogo de Instituciones y oferta formativa
+          <span className="subtitle">@quentindrphotography</span>
+        </h3>
+      </label>
+      <input
+        className="radio"
+        type="radio"
+        name="card"
+        id="card-4"
+        checked={checkedCard === "card-4"}
+        onChange={handleChange}
+      />
+      <label className="content" for="card-4">
+        <span className="icon">
+          <i className="fas fa-snowflake"></i>
+        </span>
+        <h3 className="card-title">
+          Albergues y residencias estudiantiles
+          <span className="subtitle">@punttim</span>
+        </h3>
+      </label>
+    </div>
+  );
+};
+
+export default Card;
