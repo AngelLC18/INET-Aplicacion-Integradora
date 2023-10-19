8be24c87fd913e96df8e5d8a19fbbe67a2e370fc
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2023-10-19 17:34:03 -0300

    update changes

--- src/components/main/ImageCarousel.jsx
+++ src/components/main/ImageCarousel.jsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
 
 const ImageCarousel = () => {
-  // Define el estado para el índice activo del carrusel
   const [activeIndex, setActiveIndex] = useState(0);
-
-  // Define la referencia para el contenedor del carrusel
   const carouselRef = useRef(null);
 
-  // Define el arreglo de imágenes que se mostrarán en el carrusel
+  // arreglo de imágenes que se mostrarán en el carrusel
   const images = [
     "https://cdn-0.generatormix.com/images/cats/japanese-bobtail.jpg",
     "https://cdn-0.generatormix.com/images/cats/javanese.jpg",
@@ -16,45 +13,35 @@ const ImageCarousel = () => {
     "https://cdn-0.generatormix.com/images/cats/sphynx.jpg",
   ];
 
-  // Define la función que actualiza el índice activo del carrusel
   const updateIndex = (newIndex) => {
-    // Si el nuevo índice es mayor que el número de imágenes, vuelve al inicio
     if (newIndex >= images.length) {
       newIndex = 0;
     }
-    // Si el nuevo índice es menor que cero, vuelve al final
     if (newIndex < 0) {
       newIndex = images.length - 1;
     }
-    // Actualiza el estado con el nuevo índice
     setActiveIndex(newIndex);
   };
 
-  // Define el efecto que se ejecuta cuando cambia el índice activo del carrusel
   useEffect(() => {
-    // Obtiene el ancho del contenedor del carrusel usando la referencia
     const carouselWidth = carouselRef.current.offsetWidth;
-    // Calcula la posición de desplazamiento según el índice activo y el ancho del contenedor
     const scrollPosition = activeIndex * carouselWidth;
-    // Desplaza el contenedor a la posición calculada
     carouselRef.current.scrollLeft = scrollPosition;
   }, [activeIndex]);
 
   return (
     <div className="bg-white w-[90%] h-[400px] rounded-lg mx-auto overflow-hidden relative">
-      {/* Contenedor del carrusel */}
       <div
         ref={carouselRef}
         className="h-full flex overflow-x-hidden"
         data-carousel="slide"
       >
-        {/* Elementos del carrusel */}
         {images.map((image, index) => (
           <div
             key={index}
             className={`h-full w-full flex-shrink-0 duration-700 ease-in-out  transition-opacity  ${
               index === activeIndex ? "opacity-100" : "opacity-0"
-            }`} // Aplica las clases de opacidad y duración según el índice activo
+            }`}
             data-carousel-item={index === activeIndex ? "active" : ""}
           >
             <img
@@ -65,7 +52,6 @@ const ImageCarousel = () => {
           </div>
         ))}
       </div>
-      {/* Indicadores del carrusel */}
       <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
         {images.map((image, index) => (
           <button
@@ -80,7 +66,6 @@ const ImageCarousel = () => {
           ></button>
         ))}
       </div>
-      {/* Botón anterior del carrusel */}
       <button
         type="button"
         className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer "
@@ -105,7 +90,6 @@ const ImageCarousel = () => {
           <span className="sr-only">Previous</span>
         </span>
       </button>
-      {/* Botón siguiente del carrusel */}
       <button
         type="button"
         className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer "
--- src/components/main/cards/Cards.jsx
+++ src/components/main/cards/Cards.jsx
@@ -4,8 +4,9 @@ import Card from "./card";
 const Cards = () => {
   return (
     <>
-      <section className="flex md:flex-row flex-col gap-4 overflow-x-auto items-center w-[93%] h-56 bg-slate-300 p-4 rounded-lg ">
+      <section className="flex md:flex-row flex-col gap-4 overflow-x-auto items-center w-[93%] h-56 bg-slate-300 dark:bg-slate-950 p-4 rounded-lg ">
         <Card
+          //Ingresar los atributos necesarios para configurar cara tarjeta
           text="Robotica"
           img="https://imgs.search.brave.com/VOZwSL96n-H4bcLLDaeMKyVpvnv5td3ZASVGrkOAbxI/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuc3RpY2twbmcu/Y29tL2ltYWdlcy81/ODBiNTdmYmQ5OTk2/ZTI0YmM0M2JkZjIu/cG5n"
           alt="android"
